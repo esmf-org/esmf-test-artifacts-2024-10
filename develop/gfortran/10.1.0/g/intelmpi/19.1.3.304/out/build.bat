3358cc1a2457d7b45acf469cbce4897e9c8dea11
Mark.Potts <Mark.Potts@noaa.gov>
2024-08-07 09:14:34 -0400

    action=collect dir=gfortran_10.1.0_intelmpi_g_develop branch=develop hash=v8.7.0b11 phase=build

---
+++ develop/gfortran/10.1.0/g/intelmpi/19.1.3.304/out/build.bat
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH --account=s2326
+#SBATCH -o /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load comp/gcc/10.1.0 mpi/impi/19.1.3.304
+
+set -x
+export ESMF_DIR=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=intelmpi
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop/module-build.log
+export WORK_ROOT=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop
+export TEMP_ROOT=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_10.1.0_intelmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 28 2>&1| tee $WORK_ROOT/build.log
