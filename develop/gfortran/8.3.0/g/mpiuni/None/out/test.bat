c9e4265fcd6ef7c5269c6d6bec94000e258721cc
Mark Potts <mpotts@redlineperf.com>
2024-08-14 02:23:12 -0500

    action=collect dir=gfortran_8.3.0_mpiuni_g_develop branch=develop hash=v8.7.0b11 phase=test

---
+++ develop/gfortran/8.3.0/g/mpiuni/None/out/test.bat
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop/test.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop/test.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+module load gcc/8.3.0 
+module load netcdf/4.7.4
+
+set -x
+export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop/module-test.log
+export WORK_ROOT=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop
+export TEMP_ROOT=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+make install 2>&1| tee $WORK_ROOT/install.log
+make all_tests 2>&1| tee $WORK_ROOT/test.log
+export ESMFMKFILE=`find $PWD/DEFAULTINSTALLDIR -iname esmf.mk`
