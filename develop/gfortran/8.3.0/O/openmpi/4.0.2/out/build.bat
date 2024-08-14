df6ee280d4dea6dfad07d9b5ec9bc2cb1ea0bbcb
Mark Potts <mpotts@redlineperf.com>
2024-08-14 02:21:51 -0500

    action=collect dir=gfortran_8.3.0_openmpi_O_develop branch=develop hash=v8.7.0b11 phase=build

---
+++ develop/gfortran/8.3.0/O/openmpi/4.0.2/out/build.bat
@@ -0,0 +1,35 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop/build.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake intelpython3
+module load gcc/8.3.0 openmpi/4.0.2
+module load netcdf/4.7.4
+
+set -x
+export LD_PRELOAD=/apps/gcc-8/gcc-8.3.0/lib64/libstdc++.so
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=openmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop/module-build.log
+export WORK_ROOT=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop
+export TEMP_ROOT=/work/noaa/da/mpotts/sandbox/gfortran_8.3.0_openmpi_O_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 40 2>&1| tee $WORK_ROOT/build.log
