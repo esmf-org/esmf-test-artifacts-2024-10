a874328630ae92fd0aa13ffd4ed99ce593a28440
Gerhard Theurich <theurich@sourcespring.net>
2024-09-07 09:18:24 -0700

    action=collect dir=intel_2023.1.0-oneAPI_mpiuni_O_develop branch=develop hash=v8.7.0b13-3-gedb848d phase=test

---
+++ develop/intel/2023.1.0-oneAPI/O/mpiuni/None/out/build.bat
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH --account=e3sm
+#SBATCH -o /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop/build.bat_%j.o
+#SBATCH -e /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop/build.bat_%j.e
+#SBATCH --time=2:00:00
+#SBATCH -C cpu
+#SBATCH --qos=regular
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load PrgEnv-intel cpu cmake
+module load intel-oneapi/2023.1.0 
+module load cray-hdf5-parallel cray-netcdf-hdf5parallel
+
+set -x
+export ESMF_NETCDF_LIBS="-lnetcdff -lnetcdf"
+export ESMF_DIR=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop/module-build.log
+export WORK_ROOT=/global/cfs/cdirs/e3sm/theurich/ESMF-Nightly-Testing/intel_2023.1.0-oneAPI_mpiuni_O_develop
+export TEMP_ROOT=/tmp/intel_2023.1.0-oneAPI_mpiuni_O_develop
+rsync -a $WORK_ROOT/esmf $TEMP_ROOT
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 128 2>&1| tee $WORK_ROOT/build.log
+cd
+rsync -a $TEMP_ROOT/esmf $WORK_ROOT
+rm -rf $TEMP_ROOT
