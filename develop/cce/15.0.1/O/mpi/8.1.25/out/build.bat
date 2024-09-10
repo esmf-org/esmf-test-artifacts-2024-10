791830fbcf907c48e03b57a9c256b4ccb41200ce
Bill Sacks <sacks@ucar.edu>
2024-09-10 05:38:35 -0600

    action=collect dir=cce_15.0.1_mpi_O_develop branch=develop hash=v8.7.0b13-12-g9379ba6 phase=test

---
+++ develop/cce/15.0.1/O/mpi/8.1.25/out/build.bat
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#PBS -N build.bat
+#PBS -l walltime=2:00:00
+#PBS -q main
+#PBS -A p93300606
+#PBS -l select=1:ncpus=128:mpiprocs=128
+JOBID="`echo $PBS_JOBID | cut -d. -f1`"
+
+module load ncarenv/23.06 cmake
+module load cce/15.0.1 cray-mpich/8.1.25
+module load netcdf/4.9.2
+
+set -x
+export ESMF_MPIRUN=mpiexec
+export ESMF_DIR=/glade/derecho/scratch/sacks/esmf-testing/cce_15.0.1_mpi_O_develop/esmf
+export ESMF_COMPILER=cce
+export ESMF_COMM=mpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /glade/derecho/scratch/sacks/esmf-testing/cce_15.0.1_mpi_O_develop/module-build.log
+export WORK_ROOT=/glade/derecho/scratch/sacks/esmf-testing/cce_15.0.1_mpi_O_develop
+export TEMP_ROOT=/glade/derecho/scratch/sacks/esmf-testing/cce_15.0.1_mpi_O_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 128 2>&1| tee $WORK_ROOT/build.log
