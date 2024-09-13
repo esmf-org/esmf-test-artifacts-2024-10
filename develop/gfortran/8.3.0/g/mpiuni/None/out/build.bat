dee4f880a9109e6ef4fbac557f13afe3449ea03f
Matthew Thompson <matthew.thompson@nasa.gov>
2024-09-13 14:00:08 -0400

    action=collect dir=gfortran_8.3.0_mpiuni_g_develop branch=develop hash=v8.7.0b14 phase=test

---
+++ develop/gfortran/8.3.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+#SBATCH --account=s1873
+#SBATCH -o /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH -C cas
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=45
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake
+module load comp/gcc/8.3.0 
+
+set -x
+export ESMF_DIR=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/esmf
+export ESMF_COMPILER=gfortran
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/module-build.log
+export WORK_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop
+export TEMP_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 45 2>&1| tee $WORK_ROOT/build.log
