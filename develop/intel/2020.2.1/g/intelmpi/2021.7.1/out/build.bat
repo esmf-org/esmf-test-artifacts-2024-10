4dd92a690a3b4b30ffb9cb380e671b9248e280e2
Mark Potts <mpotts@redlineperf.com>
2024-09-13 02:41:15 -0500

    action=collect dir=intel_2020.2.1_intelmpi_g_develop branch=develop hash=v8.7.0b14 phase=build

---
+++ develop/intel/2020.2.1/g/intelmpi/2021.7.1/out/build.bat
@@ -0,0 +1,34 @@
+#!/bin/bash -l
+#SBATCH --account=da-cpu
+#SBATCH -o /work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop/build.bat_%j.o
+#SBATCH -e /work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=orion
+#SBATCH --qos=batch
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module load cmake netcdf-fortran/4.6.0 python/3.10.8 intel-oneapi-mkl/2022.2.1
+module load intel-oneapi-compilers/2022.2.1 intel-oneapi-mpi/2021.7.1
+module load netcdf-c/4.9.2
+
+set -x
+export ESMF_MPIRUN=mpirun.srun
+export ESMF_DIR=/work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=intelmpi
+export ESMF_NETCDF=nc-config
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop/module-build.log
+export WORK_ROOT=/work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop
+export TEMP_ROOT=/work/noaa/da/mpotts/sandbox/intel_2020.2.1_intelmpi_g_develop
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 40 2>&1| tee $WORK_ROOT/build.log
