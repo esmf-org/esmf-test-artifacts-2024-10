425174a6bf530b75bded7bef5d73d56e0eebfb32
Mark.Potts <Mark.Potts@noaa.gov>
2024-09-19 01:55:47 -0400

    action=clear dir=intel_19.1.3_intelmpi_O_release_8.7.0 branch=release_8.7.0 hash=v8.7.0b14-1-g2597b0a phase=build

--- develop/gfortran/8.3.0/g/mpiuni/None/out/build.bat
+++ develop/gfortran/8.3.0/g/mpiuni/None/out/build.bat
@@ -1,13 +1,12 @@
 #!/bin/bash -l
-#SBATCH --account=s1873
-#SBATCH -o /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.o
-#SBATCH -e /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.e
+#SBATCH --account=s2326
+#SBATCH -o /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop/build.bat_%j.e
 #SBATCH --time=1:00:00
 #SBATCH --partition=compute
-#SBATCH -C cas
 #SBATCH --qos=allnccs
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=45
+#SBATCH --ntasks-per-node=28
 #SBATCH --exclusive
 export JOBID=$SLURM_JOBID
 
@@ -16,18 +15,18 @@ module load cmake
 module load comp/gcc/8.3.0 
 
 set -x
-export ESMF_DIR=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/esmf
+export ESMF_DIR=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop/esmf
 export ESMF_COMPILER=gfortran
 export ESMF_COMM=mpiuni
 export ESMF_BOPT='g'
 export ESMF_TESTEXHAUSTIVE='ON'
 export ESMF_TESTWITHTHREADS='ON'
-export ESMF_MPIRUN=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
-module list >& /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop/module-build.log
-export WORK_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop
-export TEMP_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/gfortran_8.3.0_mpiuni_g_develop
+export ESMF_MPIRUN=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop/module-build.log
+export WORK_ROOT=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop
+export TEMP_ROOT=/discover/nobackup/projects/sbu/mpotts/esmf-test/gfortran_8.3.0_mpiuni_g_develop
 cd $TEMP_ROOT/esmf
 export ESMF_DIR=`pwd`
 set -o pipefail
 make info 2>&1| tee $WORK_ROOT/info.log
-make -j 45 2>&1| tee $WORK_ROOT/build.log
+make -j 28 2>&1| tee $WORK_ROOT/build.log
