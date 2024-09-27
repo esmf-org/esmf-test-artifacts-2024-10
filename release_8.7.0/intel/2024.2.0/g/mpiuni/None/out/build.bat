d8dd73b419411269d29b9fed334f956c93d9a274
Matthew Thompson <matthew.thompson@nasa.gov>
2024-09-26 23:11:24 -0400

    action=collect dir=intel_2024.2.0_mpiuni_g_release_8.7.0 branch=release_8.7.0 hash=v8.7.0b14-1-g2597b0a phase=build

---
+++ release_8.7.0/intel/2024.2.0/g/mpiuni/None/out/build.bat
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH --account=s1873
+#SBATCH -o /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH -C mil
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=120
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module use /discover/swdev/gmao_SIteam/modulefiles-SLES15
+module load cmake/3.30.3 comp/gcc/12.3.0
+module load comp/intel/2024.2.0 
+
+set -x
+export I_MPI_OFI_PROVIDER=psm3
+export I_MPI_FABRICS=shm:ofi
+export ESMF_CXX=icpx
+export ESMF_CC=icx
+export ESMF_F90=ifx
+export ESMF_DIR=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0/esmf
+export ESMF_COMPILER=intel
+export ESMF_COMM=mpiuni
+export ESMF_BOPT='g'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+export ESMF_MPIRUN=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0/esmf/src/Infrastructure/stubs/mpiuni/mpirun
+module list >& /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0/module-build.log
+export WORK_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0
+export TEMP_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/intel_2024.2.0_mpiuni_g_release_8.7.0
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 120 2>&1| tee $WORK_ROOT/build.log
