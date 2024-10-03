2aae8eb3ae09d8282a62334fc114f16e65344f96
Matthew Thompson <matthew.thompson@nasa.gov>
2024-10-03 09:41:08 -0400

    action=collect dir=nag_7.2.15_openmpi_O_release_8.7.0 branch=release_8.7.0 hash=v8.7.0b14-6-gd29442c phase=test

---
+++ release_8.7.0/nag/7.2.15/O/openmpi/nag_7.2.7215-gcc_12.3.0/out/build.bat
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+#SBATCH --account=s1873
+#SBATCH -o /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0/build.bat_%j.o
+#SBATCH -e /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0/build.bat_%j.e
+#SBATCH --time=1:00:00
+#SBATCH --partition=compute
+#SBATCH -C mil
+#SBATCH --qos=allnccs
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=120
+#SBATCH --exclusive
+export JOBID=$SLURM_JOBID
+
+
+module use /discover/swdev/gmao_SIteam/modulefiles-SLES15
+module load cmake/3.30.3 comp/gcc/12.3.0
+module load comp/nag/7.2-7215 mpi/openmpi/4.1.6/nag_7.2.7215-gcc_12.3.0
+
+set -x
+export ESMF_DIR=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0/esmf
+export ESMF_COMPILER=nag
+export ESMF_COMM=openmpi
+export ESMF_BOPT='O'
+export ESMF_TESTEXHAUSTIVE='ON'
+export ESMF_TESTWITHTHREADS='ON'
+module list >& /discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0/module-build.log
+export WORK_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0
+export TEMP_ROOT=/discover/nobackup/projects/gmao/SIteam/ESMF_Testing/nag_7.2.15_openmpi_O_release_8.7.0
+cd $TEMP_ROOT/esmf
+export ESMF_DIR=`pwd`
+set -o pipefail
+make info 2>&1| tee $WORK_ROOT/info.log
+make -j 120 2>&1| tee $WORK_ROOT/build.log
